e932437ded76d67b51402cbd044eacdd765cda85
Fernando Rossin <rossinfernando@gmail.com>
2022-10-11 11:49:44 -0300

    2-Segunda entrega proyecto

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 
-// funcion de incremento de cantidad
-
-function conteo_de_cantidad(cant_prod,cant){
-        cant_prod = cant_prod + cant;
-        return cant_prod;
-}
-
 // funcion mensaje de despedida
 
 function despedida(){
@@ -14,45 +7,105 @@ function despedida(){
 
 }
 
+// función para buscar productos
+
+function filtrar_producto(arr,filtro){
+    const filtrado = arr.filter((el)=>{
+        return el.nombre.includes(filtro);
+    });
+        return filtrado;
+} 
+
+// función para crear productos
+
+function Producto(id,nombre,precio){
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+}
+
+// función para agregar producto nuevo
+
+function cargar_producto(array,prod){
+    array.push(prod)
+}
+
+
+
 // declaracion de variables
 
-let cant_remera = 0;
-let cant_pantalon = 0;
-let cant_zapatillas = 0;
-let cant_buzo = 0;
+const productos = [];
+
+const carrito = [];
+
 let continuar = 's';
 
+const producto1 = new Producto(1,'remera',1500);
+const producto2 = new Producto(2,'pantalon',3000);
+const producto3 = new Producto(3,'zapatillas',2500);
+const producto4 = new Producto(4,'buzo',2000);
 
-let inicio_sesion = prompt('Bienvenido a la tienda, desea realizar una compra? \n SI- ingrese s \n NO- ingrese n')
+cargar_producto(productos,producto1);
+cargar_producto(productos,producto2);
+cargar_producto(productos,producto3);
+cargar_producto(productos,producto4);
+
+
+let inicio_sesion = prompt('Bienvenido, desea ingresar a la tienda? \n SI- ingrese s \n NO- ingrese n')
 
 if(inicio_sesion == 's'){
 
     do{    
-        let ingreso = prompt('Que producto desea adquirir: \n 1-Remera \n 2-Pantalón \n 3-Zapatillas \n 4-Buzo');
-        let cantidad = parseInt(prompt('¿Cuantos desea comprar?'));
-
-    // Incremento de la cantidad de productos a adquirir 
+        let ingreso = prompt('Que operación desea realizar: \n 1-Búscar un producto \n 2-Compra un producto \n 3-Ver el carrito');
+        
+    // Opciones del menú: 
 
         switch (ingreso) {
             case '1':
-                cant_remera = conteo_de_cantidad(cant_remera,cantidad);
+                let buscar = prompt('Ingrese el producto que desea buscar')
+                let bus = filtrar_producto(productos,buscar);
+                for (const prod of bus) {
+                    alert('Se encontró: ' + prod.nombre + '-> Precio: '+ prod.precio);
+                }
                 break;
             case '2':
-                cant_pantalon = conteo_de_cantidad(cant_pantalon,cantidad);
+                do{    
+                    let ingreso = prompt('Que producto desea adquirir: \n 1-Remera \n 2-Pantalón \n 3-Zapatillas \n 4-Buzo');
+                    let cant = parseInt(prompt('¿Cuantos desea comprar?'));
+                    switch (ingreso) {
+                        case '1':
+                            carrito.push({id: productos[0].id,nombre: productos[0].nombre, precio: productos[0].precio, cantidad: cant});
+                            break;
+                        case '2':
+                            carrito.push({id: productos[1].id,nombre: productos[1].nombre, precio: productos[1].precio, cantidad: cant});
+                            break;
+                        case '3':
+                            carrito.push({id: productos[2].id,nombre: productos[2].nombre, precio: productos[2].precio, cantidad: cant});
+                            break;
+                        case '4':
+                            carrito.push({id: productos[3].id,nombre: productos[3].nombre, precio: productos[3].precio, cantidad: cant});
+                            break;
+                        default:
+                            break; 
+                        }
+                    continuar = prompt('desea agregar otro producto? \n SI- ingrese s \n NO- ingrese n');
+                }while(continuar != 'n');
                 break;
             case '3':
-                cant_zapatillas = conteo_de_cantidad(cant_zapatillas,cantidad);
+                if (carrito.length > 0){                
+                for (const prod of carrito) {
+                    alert('El carrito contiene: ' + prod.nombre + '-> Cantidad: '+ prod.cantidad);
+                }
                 break;
-            case '4':
-                cant_buzo = conteo_de_cantidad(cant_buzo,cantidad);
+                }else{
+                    alert('El carrito se encuentra vacío')
+                }
                 break;
             default:
                 break; 
             }
-        continuar = prompt('desea agregar otro producto? \n SI- ingrese s \n NO- ingrese n');
+        continuar = prompt('desea realizar otra consulta? \n SI- ingrese s \n NO- ingrese n');
     }while(continuar != 'n');
-    
-    alert('Carrito: \n Producto: Remera----Cantidad: '+ cant_remera + '\n Producto: pantalon---Cantidad: '+ cant_pantalon + '\n Producto: zapatillas--Cantidad: '+ cant_zapatillas + '\n Producto: buzo-------Cantidad: '+ cant_buzo);
     
     despedida();
     
